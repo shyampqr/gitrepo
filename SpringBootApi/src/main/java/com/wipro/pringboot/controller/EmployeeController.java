faabce47e751dbc8082e270e4243e6af0b4ecdce
Thenameisshyam <shyam5454@gmail.com>
2024-01-18 23:17:36 +0530

    todays controller changes

--- SpringBootApi/src/main/java/com/wipro/pringboot/controller/EmployeeController.java
+++ SpringBootApi/src/main/java/com/wipro/pringboot/controller/EmployeeController.java
@@ -16,7 +16,7 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
     @GetMapping(value = "/employees")
     public List<Employee> getAllEmployee(){
-
+        System.out.println("Adding sysout im coroller");
         return employeeRepository.findAll();
     }
     //find employee by id
